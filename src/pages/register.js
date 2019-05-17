5b71ff26baf84881c963a9688fe4dd968cfc4aa3
Michiel Van Haute <michiel@michvh.be>
2019-05-17 08:56:06 +0200

    adding base components to register page

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import LogoBlock from '../components/LogoBlock';
 
-const Register = () => <div>Register</div>;
+const Register = () => (
+  <Layout>
+    <Layout.Split>
+      <LogoBlock />
+      <Layout.Content>2</Layout.Content>
+    </Layout.Split>
+  </Layout>
+);
 
 export default Register;
